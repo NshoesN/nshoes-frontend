0b0254dc7fe3042b08e091e12823e2d0eca7909b
박찬유 <parkchanyu@chanyuPark-MacBookPro.local>
2023-10-17 23:27:49 +0900

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/Header.scss"
+import Logo from "../assets/images/NshesLogo.png"
+
+function Header() {
+    return (
+        <nav className="header-container">
+            <ul className="nav-list">
+                <li className="nav-item"><Link to><img src={Logo} alt="logo" /></Link></li>
+                <li className="nav-item"><Link to>Main</Link></li>
+                <li className="nav-item"><Link to>Market</Link></li>
+                <li className="nav-item"><Link to>New Releases</Link></li>
+                <li className="nav-item"><Link to>SNKRS</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header;
