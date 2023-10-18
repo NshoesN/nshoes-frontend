3583d70b090f1efd5a10540bc1792784ad21a289
parkchanyu <chanyu1663@naver.com>
2023-10-18 20:09:12 +0900

    UI-fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../assets/styles/Header.scss"
 import Logo from "../assets/images/NshesLogo.png"
+import Search from "../assets/icons/Search.png"
+import Shopping from "../assets/icons/shopping.png"
 
 function Header() {
     return (
@@ -12,6 +14,7 @@ function Header() {
                 <li className="nav-item"><Link to>Market</Link></li>
                 <li className="nav-item"><Link to>New Releases</Link></li>
                 <li className="nav-item"><Link to>SNKRS</Link></li>
+                <li className="nav-item"><Link to><img src={Search} alt="seach"/></Link><Link><img src={Shopping} alt="shopping" /></Link></li>
             </ul>
         </nav>
     )
