9c8e558144e6d546611d7b632ee113478d89bc5d
parkchanyu <chanyu1663@naver.com>
2023-10-19 13:49:30 +0900

    Add Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import './assets/styles/App.scss';
-import Header from './components/Header'
+import Layout from './components/Layout/Layout'
 import { Routes, Route } from 'react-router-dom'
 //Screen
 import Main from './pages/Main'
-
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Main />}></Route>
-          <Route path='/SignIn' element='<SingIn />'></Route>
-          <Route path='/SignUp' element='<SingUp />'></Route>
-          <Route path='/Cart' element='<Cart />'></Route>
-          <Route path='/Market' element='<Market />'></Route>
-          <Route path='/*' element='<NotFound />'></Route>
+        <Layout>
+          <Routes>
+            <Route path='/' exact element={<Main />}></Route>
+            <Route path='/SignIn' element={<SignIn />}></Route>
+            <Route path='/SignUp' element='<SingUp />'></Route>
+            <Route path='/Cart' element='<Cart />'></Route>
+            <Route path='/Market' element='<Market />'></Route>
+            <Route path='/*' element='<NotFound />'></Route>
 
-        </Routes>
+          </Routes>
+        </Layout>
     </div>
   );
 }
