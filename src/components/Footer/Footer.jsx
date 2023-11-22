62dc022c2d27f56908090e072de9e2c063603337
parkchanyu <chanyu1663@naver.com>
2023-11-23 02:04:58 +0900

    umm...

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+//styles
+import style from '../../assets/styles/Footer.module.scss'
+export default function Footer() {
+    return (
+        <div className={style.container}>
+            
+        </div>
+    )
+}
